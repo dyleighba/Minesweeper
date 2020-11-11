25a79caaacc289bf061c81f5a108b920b10f731a
Dylan Barber <dyleighba@gmail.com>
2020-11-12 00:06:02 +0800

    libgdx setup

---
+++ desktop/src/dev/dlbarber/minesweeper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.dlbarber.minesweeper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.dlbarber.minesweeper.MinesweeperMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MinesweeperMain(), config);
+	}
+}
